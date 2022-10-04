4a4543273b5e2b53e1caae319555b67ed3e4ad69
Pranav Joshi <79216712+MrDADDY-1@users.noreply.github.com>
2022-10-04 16:00:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("My name is Pranav");
+    System.out.println("Output after first commit");
   }
 }
